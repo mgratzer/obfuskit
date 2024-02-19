87332537fc2359949f7d79fce5ef369682267534
Martin Gratzer <martin@topmind.eu>
2024-02-20 00:18:49 +0100

    Fix obfuscator salt rendered as byte array in template

--- lib/obfuskit/obfuscator.rb
+++ lib/obfuskit/obfuscator.rb
@@ -11,23 +11,23 @@ module Obfuskit
     # The string is converted to an array of bytes and stored in @c.
     # The size of the array is stored in @l.
     def initialize(salt)
-      salt = salt.bytes if salt.is_a? String
-      @salt = salt
-      @length = @salt.size
+      @salt = salt || ""
+      @salt_bytes = (salt.bytes || []) if salt.is_a? String
+      @salt_length = @salt_bytes.size
     end
 
     # Obfuscates a string.
     # The string is converted to an array of bytes and each element is XORed with an element from @c.
     # The index of the element from @c is the index of the element from the string modulo @l.
     def obfuscate(value)
-      value.bytes.map.with_index { |b, i| b ^ @salt[i % @length] }
+      value.bytes.map.with_index { |b, i| b ^ @salt_bytes[i % @salt_length] }
     end
 
     # Reverses the obfuscation of an array of bytes.
     # Each element is XORed with an element from @c and the result is converted back to a string.
     # The index of the element from @c is the index of the element from the array modulo @l.
     def reveal(value)
-      value.map.with_index { |b, i| b ^ @salt[i % @length] }.pack('C*').force_encoding('utf-8')
+      value.map.with_index { |b, i| b ^ @salt_bytes[i % @salt_length] }.pack('C*').force_encoding('utf-8')
     end
   end
 end
